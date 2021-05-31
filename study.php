9d4975989c0f9aa1288122fe8efd51fd58cd9cee
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-05-31 18:02:28 +0900

    Add files via upload
    study_memo

--- study.php
+++ study.php
@@ -17,6 +17,14 @@
     변수 = 파일이름 :: 경로 (변수); 
     $idx = Util::sanitize($idx);
 
+    <!-- 화면에 코드 나오게 -->
+        <!-- php  -->
+        print_r($__);
+        echo $__;
+        var_dump( $__);
+
+        <!-- js -->
+        console.log($__);
 
 <!-- js -->
     정적 ex) 리스트 불러오기 전 선택삭제 or 등록 버튼처럼 고정으로 되어있는 부분
@@ -49,13 +57,6 @@
         is_notice ASC, 
         id DESC
 
-<!-- 기타 -->
-    // TODO :: 나중에 해야할 일
-    // FIXME :: 나중에 수정할 것 
-    스팸방지 → 구글참고 (API)
-    php pad zero (글자, 숫자 작성 시 남은 자리수에 무언가 채울 때 사용)
-    sql : varchar로 사용하면 0001 사용 가능 or sql 0으로 채움 기능 적용
-
 <!-- 웹에디터 (썸머노트) -->
     <script>
 
@@ -66,3 +67,39 @@
     });
 
     </script>
+
+<!-- 값을 넘길 때 js에서 -->
+    var 이름 = $(this).data("db값");
+    $("#html 값").text( $("#form name").val() );
+
+    val() > 값을 찾아옴
+    val("") > 빈값을 찾아옴
+    
+    
+<!-- 함수 -->
+    <!-- 무작위 숫자 값 나타내기  -->
+    rand(); 용어 사용  ex) 보안문자 코드, 중복되는 값을 만들어내지 않을 때 (돈과 관련되어 idx 추출할 때)
+
+    $a = rand();
+    $b = date("Ymdhis");
+    $c = $a + $b;
+    echo $c;
+
+    <!-- 마지막 idx 불러오기 -->
+    lastInsertID(); // 방금 전 Insert 로 인해 만들어진 row 의 idx를 가져온다.
+
+
+<!-- vscode 유용한 앱 -->
+    <!-- Todo Tree -->
+        // TODO :: 나중에 해야할 일 
+        // FIXME :: 나중에 수정할 것 
+
+<!-- 함수 -->
+    sorting
+
+<!-- 기타 -->
+    data-idx='${row.idx}' data에 - idx라는 이름으로 저장 '$DB idx'불러오기
+
+    스팸방지 → 구글참고 (API)
+    php pad zero (글자, 숫자 작성 시 남은 자리수에 무언가 채울 때 사용)
+    sql : varchar로 사용하면 0001 사용 가능 or sql 0으로 채움 기능 적용
