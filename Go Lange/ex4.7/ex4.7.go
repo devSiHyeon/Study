b7d03edb9b3e9cf1cd9ef01c5b85c8a14300a092
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-09-13 17:14:35 +0900

    float32 / float64 : 실수 정밀도 (2진수 정수, 실수 표현)

---
+++ Go Lange/ex4.7/ex4.7.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+func main() {
+	var a float32 = 1234.523
+	var b float32 = 3456.123
+	var c float32 = a * b
+	var d float32 = c * 3
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
+
+// 실수 : 7자리
+// float 64 : 15자리
+// 이후에는 오차가 생김 
