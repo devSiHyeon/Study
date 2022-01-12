e7303c9d53f1ad700d048647b67821bd3b673e4f
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-12 11:57:14 +0900

    header

---
+++ PHP/header.php
@@ -0,0 +1,19 @@
+<?php   
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    require_once "./DB.php";
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php_study</title>
+    <style>
+        .id_width{
+            width:300px;
+        }
+    </style>
+    <script type="text/javascript" src="./join.js"></script>
+    
+</head> 
+<body>
