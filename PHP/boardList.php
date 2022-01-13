2ce7bc00cab05f24d685af87f8d3912de36b3f90
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-13 12:20:00 +0900

    board list ( GET 방식으로 가져왔을 때 경우의 수 방지)

--- PHP/boardList.php
+++ PHP/boardList.php
@@ -2,48 +2,64 @@
 <?php 
     require_once "./header.php";
 
-    $No   = 1;
-    if (isset ($_GET['page'])){
-        $page = $_GET['page'];
-    } else {
-        $page = 1;
-    }
+    // 페이지 가져오기
+        /* 기본 페이지 
+            if (isset ($_GET['page'])){ 
+                $page = $_GET['page'];
+            } else {
+                $page = 1;
+            } 
+        */
 
-    // paging
-        // total list 
-        $sql_total	 	= "SELECT COUNT(idx) FROM member_detail";
-        $result_total	= mysqli_query ($db, $sql_total);
-        $row            = mysqli_fetch_array($result_total);
-        $row_total 	    = $row[0];  // DB row
+        // 페이지 응용
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;      // 주소창에 문자, 소수점 작성 방지하기 위해 무조건 int 형으로 바꿔준다
+
+        /* 
+            if (isset ($_GET['page'])){ 
+                $type = is_numeric($_GET['page']);
+                $page = ( $type == false ? 1 : $_GET['page']);
+            } else {
+                $page = 1;
+            }
+        */ 
         
-        // now page        
+    // 페이징
+        // 페이징 기본 list, bolck 지정       
         $page_list 	= 5;                            // 한 페이지에 몇 개의 row가 들어가는지
         $block_cnt	= 3;                            // 한 블록에 몇 개가 들어가는지
-        $block		= ceil($page / $block_cnt);     // 블록 단위
         
-        $page_start  = ($page - 1) * $page_list;  
-
-        $block_start	= (($block -1 ) * $block_cnt ) + 1;
-        $block_end	    = $block_start + $block_cnt -1 ;
+        $total_sql	 	= "SELECT COUNT(idx) FROM member_detail";
+        $total_result	= mysqli_query ($db, $total_sql);
+        $row     	    = mysqli_fetch_array($total_result);  // DB row
+        $total_row      = $row[0];
         
-        $total_page     = ceil($row_total / $page_list);    // 총 페이지 개수
-        $total_block    = ceil($total_page / $block_cnt);   // 총 블록의 개수  
+        $page_total     = ceil($total_row / $page_list);
+        $block_total    = ceil($page_total / $block_cnt);
+        
+        
+        // 페이지 예외 숫자 작성시 (page > 최대 페이지 ? 참일 경우 = 최대페이지 : 아닐 경우 (0보다 작은 숫자 일 경우 참 = 1 거짓 = 페이지 번호))
+        $page = $page > $page_total ? $page = $page_total : ($page < 0 ? $page = 1 : $page);
 
-        // 블록 마지막 번호
-        if($block_end > $total_page) {
-            $block_end = $total_page;
+        $list_start     = ($page-1) * $page_list;   // 순차적인 글 목록
+
+        $block		    = ceil($page / $block_cnt);     // 블록 시작 번호 구하기
+        $block_start    = (($block -1) * $block_cnt)+ 1;
+        $block_end      = $block_start + $block_cnt -1;
+
+        // 페이지 수와 블록 수 다르게 끝나는 경우 
+        if ( $block_end > $page_total) {
+            $block_end = $page_total;
         }
 
-    // list
-        $sql_list   = "SELECT `member_idx`, `user_name`, `user_id`, `user_phone` 
+        $list_sql   = "SELECT `member_idx`, `user_name`, `user_id`, `user_phone` 
                     FROM member_detail INNER JOIN member ON member_detail.member_idx = member.idx 
-                    ORDER BY user_name ASC, user_id ASC Limit $page_start, $page_list";
-        $result_list    = mysqli_query($db, $sql_list);
+                    ORDER BY user_name ASC, user_id ASC  LIMIT $list_start,  $page_list";
+        $list_result    = mysqli_query($db, $list_sql);
         
-    // No 순번 역순    
-    $No  = $row_total-(($page-1) * $page_list);             // DB 내용 시작 번호
-?>    
+        // 번호 역순으로
+        $No     = $total_row-(($page -1 ) *$page_list);
 
+?>    
 <a href="./index.php">메인</a>
     <table>
         <tr>
