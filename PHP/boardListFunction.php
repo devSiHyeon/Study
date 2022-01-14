b06701889c72593238db9f975373b4d7ff19e923
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-14 16:00:51 +0900

    paging ui function

--- PHP/boardListFunction.php
+++ PHP/boardListFunction.php
@@ -1,10 +1,7 @@
 <?php 
     require_once "./header.php";
-    include_once "./function.php";
-    
-    //($page, $page_list, $block_cnt, $total_row){
-    // 현재페이지, 페이지수,   블록수,   전체 게시글 수
-       
+    include "./function.php";
+           
     if (isset($_GET['page'])){
         $page = $_GET['page'] == 0 ? 1 : (int)$_GET['page'];
     } else {
@@ -15,21 +12,23 @@
     $total_result	= mysqli_query ($db, $total_sql);
     $row     	    = mysqli_fetch_array($total_result);  // DB row
     $total_row      = $row[0];
-    $page_list      = 3;
+    $page_list      = 5;
     
+    $page_total     = ceil($total_row / $page_list);
+    
+    // 페이지 예외 숫자 작성시 (page > 최대 페이지 ? 참일 경우 = 최대페이지 : 아닐 경우 (0보다 작은 숫자 일 경우 참 = 1 거짓 = 페이지 번호))
+    $page           = $page > $page_total ? $page = $page_total : ($page < 0 ? $page = 1 : $page);
     $list_start     = ($page-1) * $page_list;   // 순차적인 글 목록
 
-    // 함수 적용
-    $paging         = paging($page, 5, $page_list, $total_row);
-     // 현재페이지, 페이지수,   블록수,  전체 게시글 수, 게시판 list
-     
-     var_dump ($paging);
-    
-     $list_sql   = "SELECT `member_idx`, `user_name`, `user_id`, `user_phone` 
+    $list_sql   = "SELECT `member_idx`, `user_name`, `user_id`, `user_phone` 
              FROM member_detail INNER JOIN member ON member_detail.member_idx = member.idx 
              ORDER BY user_name ASC, user_id ASC  LIMIT $list_start, $page_list";
             
     $list_result    = mysqli_query($db, $list_sql);
+
+    // 번호 역순으로
+    $No     = $total_row-(($page -1 ) *$page_list);
+
 ?>    
 
 <a href="./index.php">메인</a>
@@ -42,48 +41,18 @@
         </tr>
     <?php     while ($list_row= mysqli_fetch_array($list_result)) {   ?> 
         <tr>
-            <td><?= $paging[0]--?></td>
+            <td><?= $No--?></td>
             <td><?= $list_row['user_id'];?></td>
             <td><?= $list_row['user_name'];?></td>
             <td><?= $list_row['user_phone'];?></td>
         </tr>
     <?php    } ?>
     </table>
+
     <?php 
-        // 함수 사용후 return 받은 값
-            $block_start    = $paging[1];
-            $block_end      = $paging[2];
-            $page_total     = $paging[3];
-
-        // 처음
-        if($page >= 1){
-            echo "<a href='./board_list_3.php?page=1'>처음  </a>";
-        }
-
-        // 이전 
-        if($page > 1){
-            $pre = $page -1 ;
-                echo "<a href='./board_list_3.php?page=$pre'><  </a>";
-        }
+        // 페이징 (함수 적용)
+        echo paging($page, $page_list, 3, $total_row);
+              // 현재페이지, 페이지수,블록수,  전체 게시글 수
         
-        // 숫자
-        for($i = $block_start; $i <= $block_end ; $i++){
-            if($page == $i){
-                echo "<a href='./board_list_3.php?page=$i'><b>$i</b></a>";
-            } else {
-                echo "<a href='./board_list_3.php?page=$i'>$i </a>";
-            }
-        }
-
-        // 다음
-        if($page < $page_total){
-            $next = $page +1 ;
-                echo "<a href='./board_list_3.php?page=$next'> >  </a>";
-        }
-
-        // 마지막
-        if($page <= $page_total){
-            echo "<a href='./board_list_3.php?page=$page_total'> 마지막</a>";
-        }
-        require_once "./footer.php"; 
+        require_once "./footer.php";  
     ?>    
