d717e488921f239058cd2569ba5b77c9a25ecb85
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-14 16:31:57 +0900

    url update

--- PHP/boardListFunction.php
+++ PHP/boardListFunction.php
@@ -1,58 +1,49 @@
 <?php 
-    require_once "./header.php";
-    include "./function.php";
-           
-    if (isset($_GET['page'])){
-        $page = $_GET['page'] == 0 ? 1 : (int)$_GET['page'];
-    } else {
-        $page = 1;
-    } 
+    function paging ($page, $page_list, $block_cnt, $total_row){
+                // 현재페이지, 페이지수,    블록수,   전체 게시글 수
     
-    $total_sql	 	= "SELECT COUNT(idx) FROM member_detail";
-    $total_result	= mysqli_query ($db, $total_sql);
-    $row     	    = mysqli_fetch_array($total_result);  // DB row
-    $total_row      = $row[0];
-    $page_list      = 5;
-    
-    $page_total     = ceil($total_row / $page_list);
-    
-    // 페이지 예외 숫자 작성시 (page > 최대 페이지 ? 참일 경우 = 최대페이지 : 아닐 경우 (0보다 작은 숫자 일 경우 참 = 1 거짓 = 페이지 번호))
-    $page           = $page > $page_total ? $page = $page_total : ($page < 0 ? $page = 1 : $page);
-    $list_start     = ($page-1) * $page_list;   // 순차적인 글 목록
-
-    $list_sql   = "SELECT `member_idx`, `user_name`, `user_id`, `user_phone` 
-             FROM member_detail INNER JOIN member ON member_detail.member_idx = member.idx 
-             ORDER BY user_name ASC, user_id ASC  LIMIT $list_start, $page_list";
-            
-    $list_result    = mysqli_query($db, $list_sql);
-
-    // 번호 역순으로
-    $No     = $total_row-(($page -1 ) *$page_list);
-
-?>    
-
-<a href="./index.php">메인</a>
-    <table>
-        <tr>
-            <td>No</td>
-            <td>아이디</td>
-            <td>이름</td>
-            <td>연락처</td>
-        </tr>
-    <?php     while ($list_row= mysqli_fetch_array($list_result)) {   ?> 
-        <tr>
-            <td><?= $No--?></td>
-            <td><?= $list_row['user_id'];?></td>
-            <td><?= $list_row['user_name'];?></td>
-            <td><?= $list_row['user_phone'];?></td>
-        </tr>
-    <?php    } ?>
-    </table>
-
-    <?php 
-        // 페이징 (함수 적용)
-        echo paging($page, $page_list, 3, $total_row);
-              // 현재페이지, 페이지수,블록수,  전체 게시글 수
+    // 페이징
+        // 페이징 기본 list, bolck 지정             
+        $page_total     = ceil($total_row / $page_list);
+        $block_total    = ceil($page_total / $block_cnt);
+        $block		    = ceil($page / $block_cnt);     // 블록 시작 번호 구하기
+        $block_start    = (($block -1) * $block_cnt)+ 1;
+        $block_end      = $block_start + $block_cnt -1;
         
-        require_once "./footer.php";  
-    ?>    
+        // 페이지 예외 숫자 작성시 (page > 최대 페이지 ? 참일 경우 = 최대페이지 : 아닐 경우 (0보다 작은 숫자 일 경우 참 = 1 거짓 = 페이지 번호))
+        $page           = $page > $page_total ? $page = $page_total : ($page < 0 ? $page = 1 : $page);
+        
+        // 페이지 수와 블록 수 다르게 끝나는 경우 
+        if ( $block_end > $page_total)  $block_end = $page_total;
+
+        $url = $_SERVER['PHP_SELF'];
+
+        // 처음
+        if($page >= 1)  echo "<a href='$url?page=1'>처음  </a>";
+        
+        // 이전 
+        if($page > 1){
+            $pre = $page -1 ;
+                echo "<a href='$url?page=$pre'>◀  </a>";
+        }
+        
+        // 숫자
+        for($i = $block_start; $i <= $block_end ; $i++){
+            if($page == $i){
+                echo "<a href='$url?page=$i'><b>$i</b></a>";
+            } else {
+                echo "<a href='$url?page=$i'>$i </a>";
+            }
+        }
+
+        // 다음
+        if($page < $page_total){
+            $next = $page +1 ;
+                echo "<a href='$url?page=$next'> ▶  </a>";
+        }
+
+        // 마지막
+        if($page <= $page_total)  echo "<a href='$url?page=$page_total'> 마지막</a>";
+        
+    }
+?>
