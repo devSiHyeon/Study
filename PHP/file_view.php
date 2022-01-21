5eebc4b09b9715ce79eec63e249b378e67464522
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-21 12:22:10 +0900

    디렉토리 파일 경로 추가

--- PHP/file_view.php
+++ PHP/file_view.php
@@ -25,17 +25,21 @@
         내용 : <textarea name="content"><?= $arr[0]['content'];?></textarea> <br>
       
     <!-- 첨부파일 -->
-    <?php
+    <?php        
+        // 디렉토리 파일 경로
+        $save_1        = $arr[0]['save'];
+        $save_2        = $arr[1]['save'];         
+        
         $file1  = $arr[0]['file_name'];
         $file2  = $arr[1]['file_name'];
-        $path_1 = "./images/$file1";
-        $path_2 = "./images/$file2";
+        $path_1 = "./$save_1/$file1";
+        $path_2 = "./$save_2/$file2";
     ?>
         파일1 : <input type="file" name="file_1" value="<?= $arr[0]['file_name'];?>">
         <?php 
             if(!empty($file1)) {                                    // DB file 있는가?
                 if (file_exists($path_1)){                          // ftp file 있는가?
-                    echo"<a href='./images/$file1' download>$file1</a>";
+                    echo"<a href='./$save_1/$file1' download>$file1</a>";
                 }
             }
         ?>  <br>
@@ -43,7 +47,7 @@
         <?php 
             if(!empty($file2)) {                                    // DB file 있는가?
                 if (file_exists($path_2)){                          // ftp file 있는가?
-                    echo"<a href='./images/$file2' download>$file2</a>";
+                    echo"<a href='./$save_2/$file2' download>$file2</a>";
                 }
             }
         ?> <br>
