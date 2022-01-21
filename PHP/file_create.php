3c9620ccadd78cb567618ac2fae9f1bdd499a9a6
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-21 12:21:22 +0900

    순서 수정

--- PHP/file_create.php
+++ PHP/file_create.php
@@ -7,43 +7,76 @@ require_once ("./DB.php");
     $title      = $_POST['title'];
     $content    = $_POST['content'];
 
-    $f1_name    = $_FILES['file_1']['name'];
-    $f1_type    = $_FILES['file_1']['type'];
+    $f1_name    = $_FILES['file_1']['name'];    
+    $f1_type  = pathinfo($_FILES['file_1']['name'], PATHINFO_EXTENSION );
     $f1_size    = $_FILES['file_1']['size'];
     $f1_t_name  = $_FILES['file_1']['tmp_name'];
     
     $f2_name    = $_FILES['file_2']['name'];
-    $f2_type    = $_FILES['file_2']['type'];
     $f2_size    = $_FILES['file_2']['size'];
-    $f2_t_name  = $_FILES['file_2']['tmp_name'];
-    //$f2_type  = pathinfo($_FILES['file_2']['name']);
+    $f2_t_name  = $_FILES['file_2']['tmp_name'];    
+    $f2_type  = pathinfo($_FILES['file_2']['name'], PATHINFO_EXTENSION );
 
     // 첨부파일 확인
-    $type   = array ("image/jpg", "image/jpeg", "image/png", "image/gif", "text/plain");
+    $type   = array ("jpg", "jpeg", "png", "gif", "txt");
     $size   = "1048576";
     
     // 디렉토리 파일 경로
     $images_file    = "./images";
     $gif_file       = "./gif";
     $png_file       = "./png";
-    $text_file      = "./text";
+    $txt_file       = "./txt";
     
     // 첨부파일 1
     if (isset($_FILES['file_1']) && strlen($f1_name) > 0){
+        // 용량, 확장자 확인
+        if ($f1_size > $size) { 
+            echo "1. 용량 확인<button onclick='history.back()'>이전</button>";
+            return;
+        }
+        if (!in_array($f1_type, $type)){    
+            echo "1 jpg, png, gif, txt 확장자만 가능합니다.<button onclick='history.back()'>이전</button>";
+            return;
+        } 
+    }
+
+    // 첨부파일 2
+    if (isset($_FILES['file_2']) && strlen($f2_name) > 0){     
+        // 용량, 확장자 확인
+        if (empty($f1_name)) {
+            echo "파일1부터 첨부하세요. <button onclick='history.back()'>이전</button>";
+            return;
+        } else {
+            if (isset($_FILES['file_2'])) { 
+                if ($f2_size > $size) {     // 용량확인
+                    echo "2. 용량 확인<button onclick='history.back()'>이전</button>";
+                    return;
+                }
+                if (!in_array($f2_type, $type)){    // 확장자 검사
+                    echo "2 jpg, png, gif, txt 확장자만 가능합니다.<button onclick='history.back()'>이전</button>";
+                    return;
+                }
+            }
+        }   
+    }
+
+    // 첨부파일 1
+    if (isset($_FILES['file_1']) && strlen($f1_name) > 0){
+
         // 첨부파일1 경로
         if(in_array($f1_type, $type)){
             switch($f1_type){
-                case "image/jpg" : case "image/jpeg":
+                case "jpg" : case "jpeg":
                     $save_1 = $images_file;
                     break;
-                case "image/gif":
+                case "gif":
                     $save_1 = $gif_file;
                     break;
-                case "image/png":
+                case "png":
                     $save_1 = $png_file;
                     break;
-                case "text/plain":
-                    $save_1 = $text_file;
+                case "txt":
+                    $save_1 = $txt_file;
                     break;
                 default:
                     $save_1 = '';
@@ -51,97 +84,70 @@ require_once ("./DB.php");
         }
         
         // directory insert
-            if (!file_exists($save_1)) {
-                //umask(0);                             // 권한 0으로 바꿔줌 (위험부담 있음)
-                mkdir($save_1, 0777, true);       // 디렉토리 생성
-                echo "1 directory Add success <br>";
-            }
+        if (!file_exists($save_1)) {
+            //umask(0);                             // 권한 0으로 바꿔줌 (위험부담 있음)
+            mkdir($save_1, 0777, true);       // 디렉토리 생성
+            echo "1 directory Add success <br>";
+        }
             
-        // 첨부파일_1 (용량, 확장자, file_name 확인)
-        if (isset($_FILES['file_1']) && strlen($f1_name) > 0){
-            if (isset($_FILES['file_1'])) {
-                if ($f1_size > $size) { // 용량확인
-                    echo "1. 용량 확인<button onclick='history.back()'>이전</button>";
-                    return;
-                }
-                if (!in_array($f1_type, $type)){    // 확장자 검사
-                    echo "1 jpg, png, gif, text 확장자만 가능합니다.<button onclick='history.back()'>이전</button>";
-                    return;
-                } 
-                if (file_exists($save_1.'/'.$f1_name)) { // 파일명 중복 확인
-                    echo $f1_name ." = file명 중복 <br>";
-                    return;
-                }
-            } 
+        // 파일명 중복 확인
+        if (file_exists($save_1.'/'.$f1_name)) { // 파일명 중복 확인
+            echo $f1_name ." = file명 중복 <br>";
+            return;
         }
     }
 
     // 첨부파일 2
-    if (isset($_FILES['file_2']) && strlen($f2_name) > 0){        
+    if (isset($_FILES['file_2']) && strlen($f2_name) > 0){     
+        
         // 첨부파일2 경로
         if(in_array($f2_type, $type)){
             switch($f2_type){
-                case "image/jpg" : case "image/jpeg":
+                case "jpg" : case "jpeg":
                     $save_2 = $images_file;
                     break;
-                case "image/gif":
+                case "gif":
                     $save_2 = $gif_file;
                     break;
-                case "image/png":
+                case "png":
                     $save_2 = $png_file;
                     break;
-                case "text/plain":
-                    $save_2 = $text_file;
+                case "txt":
+                    $save_2 = $txt_file;
                     break;
                 default:
                     $save_2 = '';
             }
         }
+
         // directory insert
-         
         if (!file_exists($save_2)) {
             //umask(0);                             // 권한 0으로 바꿔줌 (위험부담 있음)
             mkdir($save_2, 0777, true);       // 디렉토리 생성
             echo "2 directory Add success <br>";
         }
-        // 첨부파일_2 (용량, 확장자, file_name 확인)
-        if(isset($_FILES['file_2']) && strlen($f2_name) > 0){
-            if (empty($f1_name)) {
-                echo "파일1부터 첨부하세요. <button onclick='history.back()'>이전</button>";
-                return;
-            } else {
-                if (isset($_FILES['file_2'])) { // 용량확인
-                    if ($f2_size > $size) {
-                        echo "2. 용량 확인<button onclick='history.back()'>이전</button>";
-                        return;
-                    }
-                    if (!in_array($f2_type, $type)){    // 확장자 검사
-                        echo "2 jpg, png, gif, text 확장자만 가능합니다.<button onclick='history.back()'>이전</button>";
-                        return;
-                    }
-                    if (file_exists($save_2.'/'.$f2_name)) { // 파일명 중복 확인
-                        echo $f2_name ." = file명 중복 <br>";
-                        return;
-                    }
-                } 
-            }
+        // 파일명 중복 확인
+        if (file_exists($save_2.'/'.$f2_name)) { 
+            echo $f2_name ." = file명 중복 <br>";
+            return;
         }
-        // 첨부파일_1
-        if (isset($_FILES['file_1']) && strlen($f1_name) > 0) {
-            $upload = move_uploaded_file($f1_t_name,$save_1.'/'.$f1_name);
-            if($upload == false){
-                echo "파일 업로드 실패 <br>";
-                return;
-            }
-        } 
-        
-        // 첨부파일_2
-        if (isset($_FILES['file_2']) && strlen($f2_name) > 0) {
-            $upload = move_uploaded_file($f2_t_name,$save_2.'/'.$f2_name);
-            if($upload == false){
-                echo "파일 업로드 실패 <br>";
-                return;
-            }
+    }
+    
+    // 첨부파일_1
+    if (isset($_FILES['file_1']) && strlen($f1_name) > 0) {
+        $upload = move_uploaded_file($f1_t_name,$save_1.'/'.$f1_name);
+        if($upload == false){
+            echo "파일 업로드 실패 <br>";
+            return;
+        }
+    } 
+    
+    // 첨부파일_2
+    if (isset($_FILES['file_2']) && strlen($f2_name) > 0) {
+        $upload = move_uploaded_file($f2_t_name,$save_2.'/'.$f2_name);
+        if($upload == false){
+            echo "파일 업로드 실패 <br>";
+            return;
         }
     }
     // DB insert
@@ -161,5 +167,5 @@ require_once ("./DB.php");
                     VALUES ('$last_idx', '$f2_name','$f2_t_name', '$save_2','$f2_type','$ip', NOW())"; 
         $result_2   = mysqli_query($db, $sql_2);
     }
-    echo "파일 업로드 및 저장 <br>";
+    echo "파일 업로드 및 저장  <a href='./Index.php> 리스트 </a>";
 ?>
