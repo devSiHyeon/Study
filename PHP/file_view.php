501c5ac8f8256b6b34abf9a7273c6a7c090975be
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-24 16:25:46 +0900

    file_1이 있을 경우 값 hidden으로 넘기기

--- PHP/file_view.php
+++ PHP/file_view.php
@@ -18,15 +18,7 @@
         $arr[] = $row;
     }
 
-    ?>
-    <!-- 게시글 -->
-    <form action="update.php?idx=<?=$idx?>" method="POST" enctype="multipart/form-data">
-        작성자 : <?=$arr[0]['writer'];?> <br>
-        제목 : <input name="title" value="<?= $arr[0]['title'];?>"> <br>
-        내용 : <textarea name="content"><?= $arr[0]['content'];?></textarea> <br>
-      
-    <!-- 첨부파일 -->
-    <?php        
+    // 첨부파일
         // 디렉토리 파일 경로
         $save_1     = $arr[0]['save'];
         $file1      = $arr[0]['file_name'];
@@ -38,12 +30,20 @@
             $path_2     = "./$save_2/$file2";
         }
     ?>
+    
+    <!-- 게시글 -->
+    <form action="update.php?idx=<?=$idx?>" method="POST" enctype="multipart/form-data">
+        작성자 : <?=$arr[0]['writer'];?> <br>
+        제목 : <input name="title" value="<?= $arr[0]['title'];?>"> <br>
+        내용 : <textarea name="content"><?= $arr[0]['content'];?></textarea> <br>
+      
         파일1 : <input type="file" name="file_1" value="<?= $arr[0]['file_name'];?>">
         <?php 
             if(!empty($file1)) {                                    // DB file 있는가?
                 if (file_exists($path_1)){                          // ftp file 있는가?
-                    echo"<a href='./$save_1/$file1' name='file_1' download>$file1</a>";
-                }
+                    echo "<a href='./$save_1/$file1' name='file1' download>$file1</a>";
+                    echo "<input type='hidden' name='DB_file_1' value='$file1'>";
+              }
             }
             if(isset($arr[0]) && strlen($arr[0]['file_name']) > 0){  
         ?>  
@@ -55,7 +55,7 @@
         <?php 
             if(!empty($file2)) {                                    // DB file 있는가?
                 if (file_exists($path_2)){                          // ftp file 있는가?
-                    echo"<a href='./$save_2/$file2' download>$file2</a>";
+                    echo "<a href='./$save_2/$file2' download>$file2</a>";
                 }
             }
             if(isset($arr[1]) && strlen($arr[1]['file_name']) > 0){  
