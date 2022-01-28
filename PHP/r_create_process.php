71f7384d6bbdf494952721413194ee482cd2bfca
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-28 12:35:25 +0900

    DB insert 성공했을 때 alert 경로 이동

--- PHP/r_create_process.php
+++ PHP/r_create_process.php
@@ -16,5 +16,14 @@ $r_no           = $row['reply_no'] + 1;
 $sql    = "INSERT INTO reply(`detail_idx`, `reply_idx`, `reply_no`, `user_id`, `content`, `created_time`) 
 VALUES('$d_idx', '$idx', '$r_no','$user_id','$content', NOW())";
 $result = mysqli_query($db, $sql);
+$last   = mysqli_insert_id($db);
 
+if($result){
+    $sql_r    = "SELECT detail_idx FROM reply WHERE idx = '".$last."'";
+    $result_r = mysqli_query($db, $sql_r);
+    $row_r    = mysqli_fetch_assoc($result_r);
+    echo '<script>alert ("댓글 작성 완료");location.href=\'./b_view.php?idx='.$row_r['detail_idx'].'\';</script>';
+} else {
+    echo '<script>alert ("댓글 작성 오류");history.back();</script><br>';
+}
 ?>
