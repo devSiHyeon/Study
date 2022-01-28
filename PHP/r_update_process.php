1e9754328e17b949ae42a4a1fca449c51065ccdb
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-01-28 12:36:06 +0900

    Create r_update_process.php

---
+++ PHP/r_update_process.php
@@ -0,0 +1,24 @@
+<?php require_once ("./DB.php");
+
+$idx        = $_POST['reply_idx'];     // 메인 댓글 본문 idx
+$content    = $_POST['content'];             // 댓글 내용
+$user_id    = $_SESSION['user_id'];        // 댓글 작성자 id
+
+// 본문 게시판 번호 가져오기
+$sql    = "SELECT detail_idx FROM reply WHERE idx = '$idx'";
+$result = mysqli_query($db, $sql);
+$row    = mysqli_fetch_assoc($result);
+// echo $row['detail_idx'];
+
+// 댓글 내용 수정
+$sql_u    = "UPDATE reply SET `content`='$content', `updated_time`= NOW() WHERE idx = '$idx'";
+$result_u = mysqli_query($db, $sql_u);
+
+if($result_u){
+//    echo "<script>alert('수정 완료 되었습니다.');history.go(-2);</script>";
+    echo '<script>location.href=\'./b_view.php?idx='.$row['detail_idx'].'\';</script>';
+} else {
+    echo "<script>alert('수정 오류');history.back();</script>";
+}
+
+?>
