c32cb70fdf946c4c47f7661a2d9770b68435feec
SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-02-18 11:52:52 +0900

    경로 수정

--- JavaScript/login.php
+++ JavaScript/login.php
@@ -7,8 +7,8 @@
 <!DOCTYPE html>
 <head>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="./Login.js"></script>
-    <script type="text/javascript" src="./Logout.js"></script>
+    <script type="text/javascript" src="./login.js"></script>
+    <script type="text/javascript" src="./logout.js"></script>
 </head>
 <body>
     <?php 
@@ -17,14 +17,14 @@
         if (isset($_SESSION['user_id']) && strlen($_SESSION['user_id']) > 0) {
     ?>
         <?= $_SESSION['user_id']?> 님 환영합니다.
-        <form name="logout_form" action="./Logout.php" method="POST">
+        <form name="logout_form" action="./logout.php" method="POST">
             <button type="button" name='logout_btn'>로그아웃 </button>
         <form>
     <?php            
         } else {
     ?>
     ▶ 로그인<br>
-    <form name="login_form" action="./Login.php" method="POST">
+    <form name="login_form" action="./login.php" method="POST">
         id <input type="text" name="user_id" value="">
         pw <input type="password" name="user_pw" value="">
         <input type="button" name='login_btn' value="Login">
