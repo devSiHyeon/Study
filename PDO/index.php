3cd73917ba5941870e6c856bcf81ffd8e26570ff
SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2022-02-23 14:17:33 +0900

    index

---
+++ PDO/index.php
@@ -0,0 +1,31 @@
+<?php   
+    require_once "./header.php";
+    if( isset($_SESSION['user_id']) && strlen($_SESSION['user_id']) > 0){
+        echo $_SESSION['user_id']."님 환영합니다";
+?>
+    <ul>
+        <li><a href="./process/logout.php">로그아웃</a></li>
+        <li><a href="./list1.php">회원목록1 </a> : 함수 사용 & 수정 & 삭제 & 댓글</li>
+        <li><a href="./list2.php">회원목록2 </a> : 관리자, 회원 구분</li>
+        <li><a href="./boardlist.php">게시판 </a></li> 
+    </ul>
+<?php    } else { ?>
+
+    <h3>회원가입</h3>
+    <form action="./process/join.php" method="POST" onsubmit="return join_check();">                
+        <input type="text" name="user_name" value="" placeholder="이름" required>    
+        <input type="text" id="user_id" name="user_id" value="" class="id_width" placeholder="아이디 : 8~20자 첫글자는 영어로 작성해주세요" required>
+        <input type="password" name="user_pw" value="" placeholder="비밀번호" required>    
+        <input type="text" name="user_phone" value="" placeholder="연락처 : 숫자만 입력하세요" required>    
+        <input type="submit" value="회원가입">            
+    </form>
+
+    <h3>로그인</h3>
+    <form action="./process/login.php" method="POST">
+        아이디 : <input type="text" name="user_id" value="" required>
+        비밀번호 : <input type="password" name="user_pw" value="" required>
+        <input type="submit" value="로그인">
+    </form>
+<?php } 
+    require_once "./footer.php";
+?>
